2c5723bffa6e5d09976befbc1ef54a24898adecf
Ayush <ayush27gupta@gmail.com>
2024-12-14 15:38:01 +0530

    Changes for route

--- src/components/home/panels/CenterPanel.jsx
+++ src/components/home/panels/CenterPanel.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Summary from "../summary/Summary";
+import { Route, Routes } from "react-router-dom";
 
 const CenterPanel = (props) => {
   const { header } = props;
   return (
     <div className="center-main">
       <div className="center-header">{header}</div>
-      <Summary />
+      <Routes>
+        <Route path="/home" element={<Summary />} />
+      </Routes>
     </div>
   );
 };
