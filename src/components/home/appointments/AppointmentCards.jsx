fa8ebfd03de41d5f5e553c95fa2fac1d218ddd58
Ayush <ayush27gupta@gmail.com>
2024-12-14 19:16:21 +0530

    Changes

--- src/components/home/appointments/AppointmentCards.jsx
+++ src/components/home/appointments/AppointmentCards.jsx
@@ -1,9 +1,10 @@
-import React, { useRef, useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 
 const AppointmentCards = (props) => {
   const { appointCards } = props;
   const [copied, setCopied] = useState(false);
   const textAreaRef = useRef(null);
+
 
   const handleCopy = () => {
     textAreaRef.current.select();
@@ -11,6 +12,8 @@ const AppointmentCards = (props) => {
     setCopied(true);
     setTimeout(() => setCopied(false), 2000);
   };
+
+
   return (
     <div className="appointment-cards-container">
       {appointCards?.map((item, index) => (
@@ -69,7 +72,7 @@ const AppointmentCards = (props) => {
             </div>
           </div>
           <div className="appoint-call-btn">
-            <button className="appoint-book-btn">Start Call</button>
+            <button className="appoint-book-btn" disabled={index !== 0}>Start Call</button>
           </div>
         </div>
       ))}
