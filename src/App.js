a0987067d542de8a2c8060c54934583dbf6303a9
Ayush <ayush27gupta@gmail.com>
2024-12-14 14:07:31 +0530

    Added left panel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/home/panels/LeftSidePanel.jsx
+++ src/components/home/panels/LeftSidePanel.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./leftpanel.css";
 
 const LeftSidePanel = () => {
-  return (
-    <div>LeftSidePanel</div>
-  )
-}
+  const location = useLocation();
 
-export default LeftSidePanel
+  return (
+    <div className="d-flex flex-column">
+      <nav className="nav flex-column">
+        <Link
+          to="/home"
+          className={`nav-sidepanel mt-2 mb-1 ${
+            location.pathname === "/home" ? "active" : ""
+          }`}
+        >
+          Home
+        </Link>
+        <Link
+          to="/online-visits"
+          className={`nav-sidepanel mt-2 mb-1 ${
+            location.pathname === "/online-visits" ? "active" : ""
+          }`}
+        >
+          Online Visits
+        </Link>
+        <Link
+          to="/lab-reports"
+          className={`nav-sidepanel mt-2 mb-1 ${
+            location.pathname === "/lab-reports" ? "active" : ""
+          }`}
+        >
+          Lab Reports
+        </Link>
+        <Link
+          to="/prescriptions"
+          className={`nav-sidepanel mt-2 mb-1 ${
+            location.pathname === "/prescriptions" ? "active" : ""
+          }`}
+        >
+          Prescriptions
+        </Link>
+        <Link
+          to="/appointments"
+          className={`nav-sidepanel mt-2 mb-1 ${
+            location.pathname === "/appointments" ? "active" : ""
+          }`}
+        >
+          Appointments
+        </Link>
+      </nav>
+    </div>
+  );
+};
+
+export default LeftSidePanel;
